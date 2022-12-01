d51b106ce2255d7d6812a142a4681a6e65ef60c1
AmroYehya <102692470+AmroYehya@users.noreply.github.com>
2022-12-01 18:20:22 +0200

    Add files via upload

---
+++ C-PROJECT/Medical System/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define x 100
+
+void main(){
+	/*int mode,pass;
+	printf("for Admin Mode Enter 1 User Mode Enter 0 :-");
+	scanf("%d",&mode);
+	//admin mode
+	if(x == 1){
+		printf("Enter Password :-");
+		scanf("%d",&pass);
+		int i = 0;
+		while(i != 3){
+		if(pass != 1234){
+			printf("try again");
+			i++;}
+		else{
+			
+		}		//user mode
+	}else if(x == 0){
+		
+	}*/
+	int x = 10;
+	printf("%d",x);
+}
